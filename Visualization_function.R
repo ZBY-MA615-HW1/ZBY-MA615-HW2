87f267d8a56ae30a5e57dfcc6de6d57e69ffb7ec
Ingridzz <byzhang1@bu.edu>
2021-10-08 22:00:38 -0400

    MA615-HW2-ZBY

--- Visualization_function.R
+++ Visualization_function.R
@@ -1,3 +1,4 @@
+#create visualization function which in used in presentation.Rmd file
 vf<-function(input,x_tax,y_inflation){
 library(tidyverse) #load all "tidyverse" libraries
   picture<-ggplot(data=input,mapping=aes(x=x_tax,y=y_inflation,group=year,color=year))+geom_point()+xlab("tax")+ylab("inflation") 
--- Wrangling_code.R
+++ Wrangling_code.R
@@ -1,26 +1,27 @@
-
-
-library(tidyverse)
+library(tidyverse) 
 library(pacman)
 library(dplyr)
 
+# I choose two indicators: tax and inflation
 tax<-read.csv("tax_revenue_percent_of_gdp.csv",header=TRUE)
 #head(tax)
 tax<-tax%>%
-  pivot_longer(-country,names_to="year",values_to="tax")
+  pivot_longer(-country,names_to="year",values_to="tax") #tidy tax 
 
 inflation<-read.csv("inflation_annual_percent.csv",header=TRUE)
 #head(inflation)
-inflation<-inflation[,-c(2,3,4,5,6,7,8,9,10,11,12)] #delte years in "inflation"
-for (i in 2:50) {
+inflation<-inflation[,-c(2,3,4,5,6,7,8,9,10,11,12)] #delete non-repeated years in "inflation"
+for (i in 2:50) {  # transform "character" columns into "double"
 if (typeof(inflation[,i])=="character")
 inflation[,i]=as.numeric(inflation[,i])
 }
 inflation<-inflation%>%
-  pivot_longer(-country,names_to="year",values_to="inflation")
+  pivot_longer(-country,names_to="year",values_to="inflation") #tidy inflation 
 
-#same_country<-intersect(tax[,1],inflation[,1]) #countires appear in both table
-#tax_inflation<-inner_join(tax,inflation,by="country") #retai rows with same coutry??
-#tax_inflation<-tax_inflation[!duplicated(as.list(tax_inflation))]
-tax_inflation<-merge(tax,inflation)
-Afghanistan_tax_inflation<-filter(tax_inflation,country=="Afghanistan")
+same_country<-intersect(tax[,1],inflation[,1]) #countries appear in both table
+
+#compare tax and inflation  between Afghanistan and Australia
+tax_inflation<-merge(tax,inflation) #combine two table
+Afghanistan_tax_inflation<-filter(tax_inflation,country=="Afghanistan") 
+Australia_tax_inflation<-filter(tax_inflation,country=="Australia")
+
